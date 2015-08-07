9057711cf13576745858752d9eedeb1cffcfc95a
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-08-07 03:36:58 +0200

    d.ts compilation fix

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,7 @@
 /// <reference path="../vendor/typings/angularjs/angular.d.ts" />
 /// <reference path="../vendor/typings/angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="../vendor/typings/toastr/toastr.d.ts" />
+
 import * as angular from 'angular';
 import 'angular-ui-router';
 
