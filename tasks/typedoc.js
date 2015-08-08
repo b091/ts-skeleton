c4634535d4a11fcf40325ef07b3fd1fa9e8885f9
b091 <skrzypkowiak@gmail.com>
2015-08-08 14:05:30 +0200

    remove unsupported flag

--- tasks/typedoc.js
+++ tasks/typedoc.js
@@ -2,17 +2,18 @@
 module.exports = function(gulp, config) {
     return function() {
         var path = require('path');
-        var typedoc = require('gulp-typedoc');
+        var typeDoc = require('gulp-typedoc');
         var appName = require(path.join(config.projectDir, 'package.json')).name;
         var tsConfig = require(path.join(config.srcDir, 'tsconfig.json'));
 
         var compilerOptions = tsConfig.compilerOptions;
+        delete(compilerOptions.sourceMap);
         compilerOptions.out = config.docsDir;
         compilerOptions.name = appName;
         compilerOptions.mode = 'file';
 
         return gulp
             .src([config.srcDir + '**/*.ts'])
-            .pipe(typedoc(compilerOptions));
+            .pipe(typeDoc(compilerOptions));
     };
 };
