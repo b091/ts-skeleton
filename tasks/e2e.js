dbfed0a6c83dd14a4bf92ec4ebf88e0612493f32
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-08-10 15:01:10 +0200

    autoexit added

--- tasks/e2e.js
+++ tasks/e2e.js
@@ -1,18 +1,20 @@
 'use strict';
-module.exports = function (gulp, config) {
-  return function () {
-    var protractor = require('gulp-protractor').protractor;
-    var path = require('path');
+module.exports = function(gulp, config) {
+    return function() {
+        var protractor = require('gulp-protractor').protractor;
+        var path = require('path');
 
-    return gulp.src([path.join(config.testDir, 'e2e/*.js')])
-      .pipe(protractor({
-        configFile: path.join(config.testDir, 'protractor.conf.js')
-      }))
-      .on('error', function (e) {
-        throw e;
-      });
-
-  };
+        return gulp.src([path.join(config.testDir, 'e2e/*.js')])
+            .pipe(protractor({
+                configFile: path.join(config.testDir, 'protractor.conf.js')
+            }))
+            .on('end', function() {
+                console.log('E2E Testing complete');
+                process.exit();
+            })
+            .on('error', function(error) {
+                console.log('E2E Tests failed');
+                process.exit(1);
+            });
+    };
 };
-
-
