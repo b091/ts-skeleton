3695ffdfa508d9e0240c05ffd25fd009760465e0
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-08-10 23:46:50 +0200

    reformat

--- tasks/default.js
+++ tasks/default.js
@@ -1,9 +1,9 @@
 'use strict';
 module.exports = function() {
-  return function() {
-    var runSequence = require('run-sequence');
-    return runSequence('check', 'compile', 'test:unit', 'test:e2e', function() {
-      console.log('Run something else');
-    });
-  };
+    return function() {
+        var runSequence = require('run-sequence');
+        return runSequence('check', 'compile', 'test:unit', 'test:e2e', function() {
+            console.log('Done');
+        });
+    };
 };
