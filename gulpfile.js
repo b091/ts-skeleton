3c84c34d1a2581abf97f32af0bfcdfe20f5a7d3b
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-09-23 21:16:16 +0200

    ng-directive replace added for bundling html into dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ var gulp = require('gulp');
 var path = require('path');
 
 var config = {
-  projectDir: __dirname,
-  configDir: path.join(__dirname, 'config'),
-  taskDir: path.join(__dirname, 'tasks'),
-  testDir: path.join(__dirname, 'test'),
-  srcDir: path.join(__dirname, 'src'),
-  docsDir: path.join(__dirname, 'docs'),
-  distDir: path.join(__dirname, 'dist'),
-  tsLintSrcConf: path.join(__dirname, 'tslint.json'),
-  tsLintTestConf: path.join(__dirname, 'tslint.json'),
-  watchDir: path.join(__dirname, 'src')
+    projectDir: __dirname,
+    configDir: path.join(__dirname, 'config'),
+    taskDir: path.join(__dirname, 'tasks'),
+    testDir: path.join(__dirname, 'test'),
+    srcDir: path.join(__dirname, 'src'),
+    docsDir: path.join(__dirname, 'docs'),
+    distDir: path.join(__dirname, 'dist'),
+    tsLintSrcConf: path.join(__dirname, 'tslint.json'),
+    tsLintTestConf: path.join(__dirname, 'tslint.json'),
+    watchDir: path.join(__dirname, 'src')
 };
 
 gulp.task('clean:src', require('./tasks/clean')(gulp, config.srcDir));
@@ -25,7 +25,7 @@ gulp.task('compile', ['compile:src', 'compile:test']);
 
 gulp.task('serve:docs', ['build:docs'], require('./tasks/server')(gulp, config.docsDir));
 gulp.task('serve:e2e', require('./tasks/server')(gulp, __dirname, false, false));
-gulp.task('serve:dist', ['build:dist'], require('./tasks/server')(gulp, __dirname, config.watchDir, true, 'index-packed.html'));
+gulp.task('serve:dist', require('./tasks/server')(gulp, config.distDir, false));
 gulp.task('serve', ['compile:src'], require('./tasks/server')(gulp, __dirname, config.watchDir));
 
 gulp.task('check:jshint', require('./tasks/jshint')(gulp, config));
@@ -38,7 +38,8 @@ gulp.task('test:unit', require('./tasks/test')(config.testDir));
 gulp.task('test:e2e', ['serve:e2e'], require('./tasks/e2e')(gulp, config));
 gulp.task('test', ['compile', 'test:unit', 'test:e2e']);
 
-gulp.task('ng:annotate', ['compile:src'], require('./tasks/ngannotate')(gulp, config));
+gulp.task('ng:directives', ['compile:src'], require('./tasks/ngdirectives')(gulp, config));
+gulp.task('ng:annotate', ['ng:directives'], require('./tasks/ngannotate')(gulp, config));
 
 gulp.task('build:dist', ['ng:annotate'], require('./tasks/build')(gulp, config));
 gulp.task('build:docs', require('./tasks/typedoc')(gulp, config));
