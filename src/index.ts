98e05cdafba82de308f3a1bb55fa56518fc74cc0
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-09-23 21:16:29 +0200

    css added for bundling

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import "./module/menu/Menu";
 import "./module/auth/Auth";
 import "./services/EndpointService";
 
+import "./style/style.css!";
+
 App.element(document).ready(() => {
   App.bootstrap(document, [
     "app",
