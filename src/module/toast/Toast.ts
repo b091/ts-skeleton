8cf1f4aa150506012f6ee44b4bb565e2f9bb0b82
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-09-23 21:17:15 +0200

    walk around for ng-directive-replace

--- src/module/toast/Toast.ts
+++ src/module/toast/Toast.ts
@@ -6,10 +6,15 @@ const module:ng.IModule = App.module("app.toast", ["toastr"]);
 
 // @ngInject
 module.config((toastrConfig:any) => {
+  const toast:any = {};
+  toast.templateUrl = "src/module/toast/view/toast.html";
+  const progressbar:any = {};
+  progressbar.templateUrl = "src/module/toast/view/progressbar.html";
+
   App.extend(toastrConfig, {
     templates: {
-      toast: "src/module/toast/view/toast.html",
-      progressbar: "src/module/toast/view/progressbar.html"
+      toast: toast.templateUrl,
+      progressbar: progressbar.templateUrl
     }
   });
 });
