bfd927378a8918c2cdf623b3f7f5f317876c80da
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2016-01-09 00:40:31 +0100

    build task refactored

--- tasks/build.js
+++ tasks/build.js
@@ -1,25 +1,29 @@
-module.exports = (gulp, config) => {
-  return () => {
-    const path = require('path');
-    const appName = require(path.join(config.projectDir, 'package.json')).name;
-    const Builder = require('jspm').Builder;
+module.exports = (gulp, config) => () => {
+  const path = require('path');
+  const Builder = require('jspm').Builder;
+  const builder = new Builder();
 
-    const builder = new Builder();
+  return beginBuild()
+    .then(buildSFX)
+    .catch((err) => console.log('Build Failed', err));
+
+  function beginBuild() {
+    builder.reset();
+    return builder.loadConfig(path.join(config.projectDir, 'jspm.conf.js'))
+  }
+
+  function buildSFX() {
+    const appName = require(path.join(config.projectDir, 'package.json')).name;
     const distFileName = `${appName}.min.js`;
     const outFile = path.join(config.distDir, distFileName);
+    const moduleName = 'app';
+    const buildConfig = {
+      format: 'amd',
+      minify: true,
+      sourceMaps: true
+    };
+    return builder.buildStatic(moduleName, outFile, buildConfig);
+  }
 
-    builder.reset();
-    builder.loadConfig(path.join(config.projectDir, 'jspm.conf.js'))
-      .then(() => {
-        const moduleName = 'app';
-        const buildConfig = {
-          format: 'amd',
-          minify: true,
-          sourceMaps: true
-        };
-        return builder.buildStatic(moduleName, outFile, buildConfig);
-      })
-      .catch((err) => console.log('Build Failed', err));
-  };
 };
 
