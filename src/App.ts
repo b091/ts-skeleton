f9b75e9cc81f0a6ae26ccc9c7998bfab5e2c72f5
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2016-03-29 23:51:15 +0200

    dependencies update

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,12 @@
-/// <reference path="../typings/main/ambient/jquery/jquery.d.ts" />
-/// <reference path="../typings/main/ambient/angular/angular.d.ts" />
-/// <reference path="../typings/main/ambient/angular-animate/angular-animate.d.ts" />
-/// <reference path="../typings/main/ambient/angular-cookies/angular-cookies.d.ts" />
-/// <reference path="../typings/main/ambient/angular-resource/angular-resource.d.ts" />
-/// <reference path="../typings/main/ambient/angular-sanitize/angular-sanitize.d.ts" />
-/// <reference path="../typings/main/ambient/angular-translate/angular-translate.d.ts" />
-/// <reference path="../typings/main/ambient/angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="../typings/main/ambient/toastr/toastr.d.ts" />
+/// <reference path="../typings/main/ambient/jquery/index.d.ts" />
+/// <reference path="../typings/main/ambient/angular/index.d.ts" />
+/// <reference path="../typings/main/ambient/angular-animate/index.d.ts" />
+/// <reference path="../typings/main/ambient/angular-cookies/index.d.ts" />
+/// <reference path="../typings/main/ambient/angular-resource/index.d.ts" />
+/// <reference path="../typings/main/ambient/angular-sanitize/index.d.ts" />
+/// <reference path="../typings/main/ambient/angular-translate/index.d.ts" />
+/// <reference path="../typings/main/ambient/angular-ui-router/index.d.ts" />
+/// <reference path="../typings/main/ambient/toastr/index.d.ts" />
 
 /* tslint:disable:variable-name */
 
